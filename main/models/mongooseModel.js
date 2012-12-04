7af86c8393e6507c22e964b6310f1a926c0c8506
rebe100x <rebe100x@gmail.com>
2012-12-04 15:38:22 +0200

    declaration with 2 semi colons

--- main/models/mongooseModel.js
+++ main/models/mongooseModel.js
@@ -5,7 +5,7 @@
 
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema
-  , S = require('string'),
+  , S = require('string')
   , crypto = require('crypto')
   ObjectId = Schema.ObjectId;
   
